d435ce1728f298a70d0ea1175b4beeff19ef22b3
Omkar Sonawane <163371960+OmkarSonawane1230@users.noreply.github.com>
2024-06-11 15:50:53 +0000

    correct the misspell comments

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ const NAVBAR_LI_ELEMENTS = document.getElementById('navbar').firstElementChild.c
 let flag = false;
 
 
-// rotate the arrow of <li> which matches the SITE_URL
+// check for a match with the <li> element and SITE_URL, then make it active
 Array.from(NAVBAR_LI_ELEMENTS).forEach((liElement) => {
     if (liElement.firstElementChild.href == location.href) {
         liElement.classList.add('active');
@@ -11,5 +11,5 @@ Array.from(NAVBAR_LI_ELEMENTS).forEach((liElement) => {
     }
 })
 
-// if non of other is active the the Home tab is active
+// if no other tab is active, then the Home tab becomes active
 if (!flag) NAVBAR_LI_ELEMENTS[0].classList.add('active');
